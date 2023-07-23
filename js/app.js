53b64e651a6bdd0eda9bc26a9a93ab396657a9f9
mahdi habibi <info.mahdihabibi@gmail.com>
2023-07-23 06:54:53 +0330

    final project setup

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* swiper */
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    }
+});
